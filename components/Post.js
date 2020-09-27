d9ff5f1ec17e7988741ccfd63219c9f3e263fa90
jjadekwon <jjade.kwon@gmail.com>
2020-09-27 22:27:30 +0900

    #15.1 Post Component Swiper

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Image } from "react-native";
 import styled from "styled-components";
+import { Ionicons } from "@expo/vector-icons";
 import PropTypes from "prop-types";
+import Swiper from "react-native-swiper";
+import constants from "../screens/constants";
 
 const Container = styled.View``;
 
@@ -25,7 +28,16 @@ const Location = styled.Text`
   font-size: 12px;
 `;
 
-const Post = ({ user, location }) => {
+const IconsContainer = styled.View`
+  padding: 10px;
+  flex-direction: row;
+`;
+
+const IconContainer = styled.View`
+  margin-right: 10px;
+`;
+
+const Post = ({ user, location, files = [] }) => {
   return (
     <Container>
       <Header>
@@ -42,6 +54,40 @@ const Post = ({ user, location }) => {
           </HeaderUserContainer>
         </Touchable>
       </Header>
+      <Swiper
+        showsPagination={false}
+        style={{ height: constants.height / 2.5 }}
+      >
+        {files.map((file) => (
+          <Image
+            key={file.id}
+            style={{ width: constants.width, height: constants.height / 2.5 }}
+            source={{ uri: file.url }}
+          />
+        ))}
+      </Swiper>
+      <IconsContainer>
+        <Touchable>
+          <IconContainer>
+            <Ionicons
+              size={28}
+              name={
+                Platform.OS === "ios" ? "ios-heart-empty" : "md-heart-empty"
+              }
+            />
+          </IconContainer>
+        </Touchable>
+        <Touchable>
+          <IconContainer>
+            <Ionicons
+              size={28}
+              name={
+                Platform.OS === "ios" ? "ios-heart-empty" : "md-heart-empty"
+              }
+            />
+          </IconContainer>
+        </Touchable>
+      </IconsContainer>
     </Container>
   );
 };
