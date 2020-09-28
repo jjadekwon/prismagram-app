768a996aae12a8c9654a6fa59e3a83b910e9b61d
jjadekwon <jjade.kwon@gmail.com>
2020-09-28 21:30:40 +0900

    #16.3 Search Query

--- screens/Search.js
+++ screens/Search.js
@@ -1,6 +1,22 @@
 import React, { useState, useEffect } from "react";
+import { ScrollView, RefreshControl } from "react-native";
 import styled from "styled-components";
 import SearchBar from "../components/SearchBar";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
+
+const SEARCH = gql`
+  query search($term: String!) {
+    searchPost(term: $term) {
+      id
+      files {
+        url
+      }
+      likeCount
+      commentCount
+    }
+  }
+`;
 
 const View = styled.View`
   justify-content: center;
@@ -12,26 +28,51 @@ const Text = styled.Text``;
 
 export default ({ navigation }) => {
   const [term, setTerm] = useState("");
+  const [shouldFetch, setShouldFetch] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const { data, loading, refetch } = useQuery(SEARCH, {
+    variables: {
+      term,
+    },
+    skip: !shouldFetch,
+  });
+
   const handleOnChange = (text) => {
     setTerm(text);
+    setShouldFetch(false);
   };
 
   const handleOnSubmit = () => {
+    setShouldFetch(true);
   };
 
-  navigation.setOptions({
-    headerTitle: () => (
-      <SearchBar
-        value={term}
-        onChange={handleOnChange}
-        onSubmit={handleOnSubmit}
-      />
-    ),
-  });
+  useEffect(() =>
+    navigation.setOptions({
+      headerTitle: () => (
+        <SearchBar
+          value={term}
+          onChange={handleOnChange}
+          onSubmit={handleOnSubmit}
+        />
+      ),
+    })
+  );
+
+  const onRefresh = async () => {
+    try {
+      setRefreshing(true);
+      await refetch({ variables: { term } });
+    } catch (e) {
+    } finally {
+      setRefreshing(false);
+    }
+  };
 
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl onRefresh={onRefresh} refreshing={refreshing} />
+      }
+    ></ScrollView>
   );
 };
