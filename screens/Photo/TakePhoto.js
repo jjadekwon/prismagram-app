d4e6494b50b9b410dcdb25203aaa0b8502c3b2cd
jjadekwon <jjade.kwon@gmail.com>
2020-09-30 22:50:31 +0900

    #18.4 Take Photo

--- screens/Photo/TakePhoto.js
+++ screens/Photo/TakePhoto.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import * as Permissions from "expo-permissions";
 import { Camera } from "expo-camera";
 import styled from "styled-components";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, Platform } from "react-native";
 import Loader from "../../components/Loader";
 import constants from "../../constants";
+import { Ionicons } from "@expo/vector-icons";
+import styles from "../../styles";
 
 const View = styled.View`
   flex: 1;
@@ -13,6 +15,7 @@ const View = styled.View`
 export default ({ navigation }) => {
   const [loading, setLoading] = useState(true);
   const [hasPermissions, setHasPermissions] = useState(false);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
   const askPermission = async () => {
     try {
       const { status } = await Permissions.askAsync(Permissions.CAMERA); // 권한 요청
@@ -27,6 +30,14 @@ export default ({ navigation }) => {
     }
   };
 
+  const toggleType = () => {
+    if (cameraType === Camera.Constants.Type.front) {
+      setCameraType(Camera.Constants.Type.back);
+    } else {
+      setCameraType(Camera.Constants.Type.front);
+    }
+  };
+
   useEffect(() => {
     askPermission();
   }, []);
@@ -37,8 +48,26 @@ export default ({ navigation }) => {
         <Loader />
       ) : hasPermissions ? (
         <Camera
-          style={{ width: constants.width, height: constants.height / 2 }}
-        />
+          type={cameraType}
+          style={{
+            justifyContent: "flex-end",
+            padding: 15,
+            width: constants.width,
+            height: constants.height / 2,
+          }}
+        >
+          <TouchableOpacity onPress={toggleType}>
+            <Ionicons
+              name={
+                Platform.OS === "ios"
+                  ? "ios-reverse-camera"
+                  : "md-reverse-camera"
+              }
+              size={32}
+              color={styles.blackColor}
+            />
+          </TouchableOpacity>
+        </Camera>
       ) : null}
     </View>
   );
